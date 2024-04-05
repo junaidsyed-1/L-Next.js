50cb083823976ac31827f8581b6f6f44b59dd210
junaid <jaanusd79@gmail.com>
2024-04-06 00:56:25 +0530

    Dynamic Route, Catch All Segments

---
+++ app/docs/[...slugs]/page.jsx
@@ -0,0 +1,23 @@
+export default function Docs({ params }) {
+  console.log(params);
+  if (params.slugs.length === 1) {
+    return (
+      <div>
+        <h1>Reviewing Docs for {params.slugs[0]}</h1>
+      </div>
+    );
+  } else if (params.slugs.length === 2) {
+    return (
+      <div>
+        <h1>
+          Reviewing Docs for {params.slugs[0]} and for {params.slugs[1]}
+        </h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>Welcome to Docs</h1>
+    </div>
+  );
+}
