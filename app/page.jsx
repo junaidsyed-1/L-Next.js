313f4b495318a0e6af3bcd766b988591bc8f95d5
junaid <jaanusd79@gmail.com>
2024-04-07 04:08:01 +0530

    Updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
-
 export default function Home() {
   return (
     <>
-      <h2>Hello world</h2>
-      <Link href='/server/users'><button className='bg-red-700 p-2 m-2 text-white rounded-lg hover:bg-opacity-90 '>Go to server/users</button></Link>
+      <Link href="/server/users">
+        <button className="bg-red-700 p-2 m-2 text-white rounded-lg hover:bg-opacity-90 ">
+          Go to server/users
+        </button>
+      </Link>
     </>
   );
 }
